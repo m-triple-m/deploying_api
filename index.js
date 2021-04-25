4775d91779ded0fd42effa1c3beb3ef430bb7f2c
Mohammad-Mubassir <triplem656@gmail.com>
2021-04-25 15:49:50 +0530

    ..

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
-const app = require('express')();
+const express = require('express')
+const app = express();
 const port = process.env.PORT || 3000;
+
+app.use(express.static("public"))
 
 app.get('/', (req, res) => {
     console.log('a request!!');
